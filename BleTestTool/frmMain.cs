5af3094da74ba18f9fba8f3d371eb7354ae474ba
Chishin <pro@xxgzs.org>
2018-05-07 18:24:46 +0800

    修复发送设备命令错误（#1-1）

--- BleTestTool/frmMain.cs
+++ BleTestTool/frmMain.cs
@@ -285,7 +285,7 @@ namespace BleTestTool
         private void toolCmdWrite_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
             Console.WriteLine("选择快捷命令：" + e.ClickedItem.Name + " - " + e.ClickedItem.Text);
-            byte[] buffer = { 0x52, 0x02, 0x02, 0x10, 0x00, 0x00 };
+            byte[] buffer = { 0x52, 0x02, 0x01, 0x10, 0x00, 0x00 };
             buffer[4] = Convert.ToByte(Convert.ToInt32(e.ClickedItem.Tag));
             buffer[buffer.Length - 1] = ByteCheakSum(buffer);
             Console.WriteLine(SerialData.ToHexString(buffer));
