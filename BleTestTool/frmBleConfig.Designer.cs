8d07866d14c7d8cd0f5b54701f14ca4223806040
Chishin <pro@xxgzs.org>
2018-05-13 22:37:02 +0800

    增加蓝牙名称替换功能

--- BleTestTool/frmBleConfig.Designer.cs
+++ BleTestTool/frmBleConfig.Designer.cs
@@ -28,62 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listViewBleNameReplace = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBleName = new System.Windows.Forms.TextBox();
+            this.txtBleReplace = new System.Windows.Forms.TextBox();
+            this.btnBleNameReplace = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // groupBox1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(388, 377);
-            this.tabControl1.TabIndex = 0;
+            this.groupBox1.Controls.Add(this.btnBleNameReplace);
+            this.groupBox1.Controls.Add(this.txtBleReplace);
+            this.groupBox1.Controls.Add(this.txtBleName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.listViewBleNameReplace);
+            this.groupBox1.Location = new System.Drawing.Point(14, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.groupBox1.Size = new System.Drawing.Size(372, 231);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "蓝牙名称替换";
             // 
-            // tabPage1
+            // groupBox2
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(380, 351);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.groupBox2.Location = new System.Drawing.Point(14, 266);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(374, 150);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "蓝牙黑名单";
             // 
-            // tabPage2
+            // listViewBleNameReplace
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.listViewBleNameReplace.Location = new System.Drawing.Point(8, 92);
+            this.listViewBleNameReplace.Name = "listViewBleNameReplace";
+            this.listViewBleNameReplace.Size = new System.Drawing.Size(356, 131);
+            this.listViewBleNameReplace.TabIndex = 0;
+            this.listViewBleNameReplace.UseCompatibleStateImageBehavior = false;
+            this.listViewBleNameReplace.View = System.Windows.Forms.View.Details;
+            this.listViewBleNameReplace.SelectedIndexChanged += new System.EventHandler(this.listViewBleNameReplace_SelectedIndexChanged);
+            this.listViewBleNameReplace.DoubleClick += new System.EventHandler(this.listViewBleNameReplace_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "蓝牙名称：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "替换内容：";
+            // 
+            // txtBleName
+            // 
+            this.txtBleName.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.txtBleName.Location = new System.Drawing.Point(92, 30);
+            this.txtBleName.Name = "txtBleName";
+            this.txtBleName.Size = new System.Drawing.Size(182, 25);
+            this.txtBleName.TabIndex = 3;
+            // 
+            // txtBleReplace
+            // 
+            this.txtBleReplace.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.txtBleReplace.Location = new System.Drawing.Point(92, 61);
+            this.txtBleReplace.Name = "txtBleReplace";
+            this.txtBleReplace.Size = new System.Drawing.Size(182, 25);
+            this.txtBleReplace.TabIndex = 4;
+            // 
+            // btnBleNameReplace
+            // 
+            this.btnBleNameReplace.Location = new System.Drawing.Point(280, 30);
+            this.btnBleNameReplace.Name = "btnBleNameReplace";
+            this.btnBleNameReplace.Size = new System.Drawing.Size(84, 56);
+            this.btnBleNameReplace.TabIndex = 5;
+            this.btnBleNameReplace.Text = "添加";
+            this.btnBleNameReplace.UseVisualStyleBackColor = true;
+            this.btnBleNameReplace.Click += new System.EventHandler(this.btnBleNameReplace_Click);
             // 
             // frmBleConfig
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(412, 401);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(400, 449);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
             this.Name = "frmBleConfig";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "蓝牙高级配置";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBleConfig_FormClosing);
             this.Load += new System.EventHandler(this.frmBleConfig_Load);
-            this.tabControl1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listViewBleNameReplace;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnBleNameReplace;
+        private System.Windows.Forms.TextBox txtBleReplace;
+        private System.Windows.Forms.TextBox txtBleName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- BleTestTool/frmBleConfig.cs
+++ BleTestTool/frmBleConfig.cs
@@ -16,6 +16,8 @@ namespace BleTestTool
         /// </summary>
         private BleConfig bleConfig;
 
+        #region 加载页面
+
         public frmBleConfig(BleConfig bc)
         {
             InitializeComponent();
@@ -35,6 +37,10 @@ namespace BleTestTool
             {
                 Console.WriteLine(item.Key + " : " + item.Value);
             }
+
+            //初始化列表
+            InitBleNameReplaceList(listViewBleNameReplace);
+            InitBleBlackList();
         }
 
         private void frmBleConfig_FormClosing(object sender, FormClosingEventArgs e)
@@ -42,5 +48,141 @@ namespace BleTestTool
             this.Hide();
             e.Cancel = false;
         }
+
+        private void InitBleNameReplaceList(ListView listView)
+        {
+            //基本属性设置
+            listView.FullRowSelect = true;
+            listView.GridLines = true;
+            listView.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+            listView.View = View.Details;
+
+            //创建列表头
+            listView.Columns.Add("蓝牙名称", 170, HorizontalAlignment.Left);
+            listView.Columns.Add("替换内容", 170, HorizontalAlignment.Left);
+
+            //添加数据
+            listView.BeginUpdate();
+            foreach (KeyValuePair<string,string> item in bleConfig.DicBleNameReplaceConfig)
+            {
+                ListViewItem listViewItem = new ListViewItem();
+                listViewItem.Text = item.Key;
+                listViewItem.SubItems.Add(item.Value);
+                listView.Items.Add(listViewItem);
+            }
+            listView.EndUpdate();
+        }
+
+        private void InitBleBlackList()
+        {
+
+        }
+
+        #endregion
+
+        #region 表格操作
+        /// <summary>
+        /// 添加List数据
+        /// </summary>
+        /// <param name="listView">表格</param>
+        /// <param name="key">数据</param>
+        /// <param name="value">数据</param>
+        private void AddListData(ListView listView, string key, string value)
+        {
+            ListViewItem listViewItem = new ListViewItem();
+            listViewItem.Text = key;
+            listViewItem.SubItems.Add(value);
+            listView.Items.Add(listViewItem);
+        }
+
+        /// <summary>
+        /// 编辑List数据
+        /// </summary>
+        /// <param name="listView">表格</param>
+        /// <param name="key">数据(唯一标识)</param>
+        /// <param name="value">数据</param>
+        private void EditListData(ListView listView, string key, string value)
+        {
+            foreach (ListViewItem item in listView.Items)
+            {
+                if (item.Text == key)
+                {
+                    item.SubItems[1].Text = value;
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 删除List数据
+        /// </summary>
+        /// <param name="listView">表格</param>
+        /// <param name="index">标号</param>
+        private void DeleteListData(ListView listView, int index)
+        {
+            if (listView.Items.Count > index)
+            {
+                listView.Items[index].Remove();
+            }
+        }
+        #endregion
+
+        #region 蓝牙名称替换控件
+        private void btnBleNameReplace_Click(object sender, EventArgs e)
+        {
+            string key = txtBleName.Text.Trim();
+            string value = txtBleReplace.Text.Trim();
+            if (key != "" && value != "")
+            {
+                if (!bleConfig.DicBleNameReplaceConfig.ContainsKey(key))
+                {
+                    //添加数据
+                    AddListData(listViewBleNameReplace, key, value);
+                    bleConfig.DicBleNameReplaceConfig.Add(key, value);
+                    bleConfig.SaveBleNameReplaceConfig();
+                }
+                else
+                {
+                    //编辑数据
+                    EditListData(listViewBleNameReplace, key, value);
+                    bleConfig.DicBleNameReplaceConfig[key] = value;
+                    bleConfig.SaveBleNameReplaceConfig();
+                }
+            }
+            else
+            {
+                MessageBox.Show("内容不可为空！", "添加失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listViewBleNameReplace_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (((ListView)sender).SelectedItems.Count > 0)
+            {
+                txtBleName.Text = ((ListView)sender).SelectedItems[0].Text;
+                txtBleReplace.Text = ((ListView)sender).SelectedItems[0].SubItems[1].Text;
+            }
+            else
+            {
+                txtBleName.Text = "";
+                txtBleReplace.Text = "";
+            }
+        }
+
+        private void listViewBleNameReplace_DoubleClick(object sender, EventArgs e)
+        {
+            ListView listView = (ListView)sender;
+            if (listView.SelectedItems.Count > 0)
+            {
+                bleConfig.DicBleNameReplaceConfig.Remove(listView.SelectedItems[0].Text);
+                bleConfig.SaveBleNameReplaceConfig();
+                DeleteListData(listView, listView.SelectedItems[0].Index);
+                //listView.SelectedItems[0].Remove();
+            }
+        }
+
+        #endregion
+
+
     }
 }
