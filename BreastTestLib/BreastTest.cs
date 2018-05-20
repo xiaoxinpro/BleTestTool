6994176e42ac2be4913868f77d55cd09b8524514
Chishin <pro@xxgzs.org>
2018-05-20 17:21:11 +0800

    增加丰胸仪测试驱动库

---
+++ BreastTestLib/BreastTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DeviceTestLib
+{
+    public class DeviceTestClass : IDeviceTest
+    {
+        public ToolStrip ToolCmdWrite { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ListView ListViewSerialReceived { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ListView ListViewBleTest { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Label LabelBleTestStatus { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event DelegateAddCmdWrite EventAddCmdWrite;
+
+        public void BytesReceviedDataProcess(byte[] arrData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitDeviceTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
